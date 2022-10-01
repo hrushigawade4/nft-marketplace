15d964c005eff2d07d29e478e757314b05618ab3
Hrushikesh Gawade <88044388+hrushigawade4@users.noreply.github.com>
2022-10-01 17:16:53 +0530

    first comit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+
+const projectId = "afab296baa5a486b9d8366c6fda0150b";
+// const projectId = "2FX0ZxkFt5qw44ZiJ1m9xnHhuoH";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3//${projectId}`,
+      accounts: [privateKey],
+    },
+  },
+  solidity: "0.8.4",
 };
